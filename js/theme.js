8eec2034bf190c67865aa6c89acbdf9c7a1d16da
MMohmedMahmoudd <162662856+MMohmedMahmoudd@users.noreply.github.com>
2024-10-21 23:12:37 +0300

    first commit

---
+++ js/theme.js
@@ -0,0 +1,393 @@
+;(function($) {
+    "use strict"; 
+    
+    //* Navbar Fixed  
+    function navbarFixed(){
+        if ( $('.main_header_area').length ){ 
+            $(window).on('scroll', function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= 295) {
+                    $(".main_header_area").addClass("navbar_fixed");
+                } else {
+                    $(".main_header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };  
+    
+    /* Main Slider js */
+    function main_slider(){
+        if ( $('#main_slider').length ){
+            $("#main_slider").revolution({
+                sliderType:"standard",
+                sliderLayout:"auto",
+                delay:9000,
+                disableProgressBar:"off",  
+                navigation: {
+                    onHoverStop: 'off',
+                    touch:{
+                        touchenabled:"on"
+                    },
+                    arrows: {
+                        style:"zeus",
+                        enable:true,
+                        hide_onmobile:true,
+                        hide_under:767,
+                        hide_onleave:true,
+                        hide_delay:200,
+                        hide_delay_mobile:1200,
+                        tmp:'<div class="tp-title-wrap"><div class="tp-arr-imgholder"></div></div>',
+                        left: {
+                            h_align: "left",
+                            v_align: "center",
+                            h_offset: 30,
+                            v_offset: 0
+                        },
+                        right: {
+                            h_align: "right",
+                            v_align: "center",
+                            h_offset: 30,
+                            v_offset: 0
+                        }
+                    },
+                },
+                responsiveLevels:[4096,1199,992,767,540],
+                gridwidth:[1170,1000,750,700,500],
+                gridheight:[985,900,650,500,500],
+                lazyType:"smart", 
+                fallbacks: {
+                    simplifyAll:"off",
+                    nextSlideOnWindowFocus:"off",
+                    disableFocusListener:false,
+                }
+            })
+        }
+    };
+    
+    //* Magnificpopup js
+    function magnificPopup() {
+        if ($('.slider_carousel, .fetured_project').length) { 
+            //Video Popup
+            $('.popup-youtube').magnificPopup({
+                disableOn: 700,
+                type: 'iframe',
+                mainClass: 'mfp-fade',
+                removalDelay: 160,
+                preloader: false, 
+                fixedContentPos: false,
+            });  
+            // Image popups
+            $('.project_img').magnificPopup({
+                delegate: 'a',
+                type: 'image',
+                removalDelay: 500,
+                callbacks: {
+                    beforeOpen: function () { 
+                        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                        this.st.mainClass = this.st.el.attr('data-effect');
+                    }
+                },
+                closeOnContentClick: true,
+                midClick: true
+            });
+        };
+    }; 
+     
+    //* Counter Js 
+    function counterUp(){
+        if ( $('.experience_area').length ){ 
+            $('.counter').counterUp({
+                delay: 10,
+                time: 400
+            });
+        };
+    }; 
+     
+    //* testimonialLogo Js 
+    function testimonialLogo(){
+        if ( $('.testimonial_logo').length ){ 
+            $('.testimonial_logo').owlCarousel({
+                loop: true,
+                margin: 95,
+                nav: false,
+                items: 4,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    400: {
+                        items: 2
+                    },
+                    600: {
+                        items: 3
+                    },
+                    800: {
+                        items: 4
+                    },
+                    1100: {
+                        items: 5
+                    }
+                }
+            })
+        };
+    }; 
+     
+    
+    // Scroll to top
+    function scrollToTop() {
+        if ($('.scroll-top').length) {  
+            $(window).on('scroll', function () {
+                if ($(this).scrollTop() > 200) {
+                    $('.scroll-top').fadeIn();
+                } else {
+                    $('.scroll-top').fadeOut();
+                }
+            }); 
+            //Click event to scroll to top
+            $('.scroll-top').on('click', function () {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 1000);
+                return false;
+            });
+        }
+    }  
+    
+    //*  Google map js 
+    if ( $('#mapBox').length ){
+        var $lat = $('#mapBox').data('lat');
+        var $lon = $('#mapBox').data('lon');
+        var $zoom = $('#mapBox').data('zoom');
+        var $marker = $('#mapBox').data('marker');
+        var $info = $('#mapBox').data('info');
+        var $markerLat = $('#mapBox').data('mlat');
+        var $markerLon = $('#mapBox').data('mlon');
+        var map = new GMaps({
+        el: '#mapBox',
+        lat: $lat,
+        lng: $lon,
+        scrollwheel: false,
+        scaleControl: true,
+        streetViewControl: false,
+        panControl: true,
+        disableDoubleClickZoom: true,
+        mapTypeControl: false,
+        zoom: $zoom,
+            styles: [
+                {
+                    "featureType": "water",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#dcdfe6"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "color": "#808080"
+                        },
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#dcdfe6"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#ffffff"
+                        },
+                        {
+                            "weight": 1.8
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "color": "#d7d7d7"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#ebebeb"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#a7a7a7"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#efefef"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#696969"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#737373"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "color": "#d6d6d6"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {},
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#dadada"
+                        }
+                    ]
+                }
+            ]
+        });
+
+        map.addMarker({
+            lat: $markerLat,
+            lng: $markerLon,
+            icon: $marker,    
+            infoWindow: {
+              content: $info
+            }
+        })
+    };
+    
+    // Preloader JS
+    function preloader(){
+        if( $('#preloader').length ){
+            $(window).on('load', function() {
+                $('#preloader').fadeOut();
+                $('#preloader').delay(50).fadeOut('slow');  
+            })   
+        }
+    }
+    
+    /*Function Calls*/
+    new WOW().init();
+    main_slider ();
+    navbarFixed (); 
+    magnificPopup ();
+    counterUp ();
+    testimonialLogo ();
+    scrollToTop ();
+    preloader (); 
+    
+})(jQuery);
